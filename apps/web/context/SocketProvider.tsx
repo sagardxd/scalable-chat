465d9c83b9c06fb99def752b58668a4ff1ce73d2
Sagar Yadav <sagardxd5@gmail.com>
2024-02-27 16:47:40 +0530

    Message emit

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useCallback, useContext, useEffect } from "react"
-import { io } from 'socket.io-client'
+import React, { useCallback, useContext, useEffect, useState } from "react"
+import { io , Socket} from 'socket.io-client'
 
 interface SocketProviderProps {
     children?: React.ReactNode
@@ -19,16 +19,21 @@ export const useSocket = () => {
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
-
+    const [socket, setSocket] = useState<Socket>();
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log('Send message', msg)
-    }, []);
+        if(socket){
+            socket.emit('event:message', {message: msg})
+        }
+    }, [socket]);
 
     useEffect(() => {
         const _socket = io('http://localhost:8000');
+        setSocket(_socket);
 
         return () => {
             _socket.disconnect();
+            setSocket(undefined)
         }
     }, []);
 
