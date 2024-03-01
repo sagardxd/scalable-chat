956c68271ed37f1e2da93613a855dee5bd097d02
Sagar Yadav <sagardxd5@gmail.com>
2024-03-01 18:42:20 +0530

    prisma

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,5 +1,6 @@
 import {Server} from "socket.io"
 import Redis from 'ioredis'
+import prismaClient from "./prisma";
 
 const pub = new Redis({
     host: "redis-768c9ab-sagardxd.a.aivencloud.com",
@@ -41,9 +42,14 @@ class SocketService {
             })
         });
 
-        sub.on('message', (channel,message) => {
+        sub.on('message', async (channel,message) => {
             if(channel === "MESSAGES") {
                 io.emit("message", message);
+                await prismaClient.message.create({
+                    data: {
+                        text:message,
+                    }
+                })
             }
         })
     }
